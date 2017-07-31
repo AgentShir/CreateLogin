0b97f887fcfe37fa3a966f32a00f630c59e0c3ce
Shireen Garcia <garciasal@gmail.com>
2017-07-31 15:36:55 -0700

    Form added

--- app.js
+++ app.js
@@ -14,6 +14,46 @@ app.get("/", function(req, res, next){
   res.render("index", {appType:"Express"})
 })
 
+var html = '<form action="/" method="post">' +
+          '<input type="text" name="username" placeholder="Username" />' +
+          '<input type="password" name="password" placeholder="Password" />' +
+          '<button type="submit">Submit</button>' +
+      '</form>';
+res.send(html);
+})
+
+// var app = express()
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(function (req, res, next) {
+  var views = req.session.views
+
+  if (!views) {
+    views = req.session.views = {}
+  }
+
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+
+  // count the views
+  views[pathname] = (views[pathname] || 0) + 1
+
+  next()
+})
+
+app.get('/foo', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+})
+
+app.get('/bar', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+})
+
 app.listen(3000, function(){
   console.log("App running on port 3000")
 })
